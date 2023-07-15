e2b36e21661c4906ec43b1f689b2460538bf083c
raiana773 <104357057+raiana773@users.noreply.github.com>
2023-07-15 20:18:27 +0545

    leksia

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// console.log('test');
+
+// let name = "John";
+
+// function makeLowerCaseAndReverse (str) {
+//     return str.split("").reverse().join("").toLowerCase();
+// }
+// const updatedName = makeLowerCaseAndReverse(name);
+// console.log(updatedName);
+
+// ? ЭКСТРА ТАСК 3
+// let data = 10;
+// if(typeof data === 'number'){
+//     console.log(`${data} - это число`)
+// }else if(typeof data == 'string'){
+//     console.log(`${data}- это строка, в которой лежит число `)
+// }else{
+//     console.log(`Необработанный случай`)
+// }
+
+
